efc47bb3f184945d7a51bf0e805ed2a4a30f4f2a
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-01-29 10:30:35 +0200

    Add SmartInput component and AddPet page

---
+++ src/pages/AddPet.jsx
@@ -0,0 +1,36 @@
+import { useFormik } from 'formik';
+import { Link } from 'react-router-dom';
+import SmartInput from '../components/UI/SmartInput';
+
+const samplePet = {
+  name: 'Lese',
+  dob: 1698883200000,
+  client_email: 'lese@gmail.com',
+};
+
+export default function AddPet() {
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      dob: '',
+      client_email: '',
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+  console.log('formik.values ===', formik.values);
+  return (
+    <div className='container'>
+      <h1>AddPet</h1>
+      <Link to='/'>Go back</Link>
+      <hr />
+      <form onSubmit={formik.handleSubmit}>
+        <SmartInput label='Name' name='name' formik={formik} />
+        <SmartInput type='date' label='Date of birth' name='dob' formik={formik} />
+        <SmartInput label='Client email' name='client_email' formik={formik} />
+        <button type='submit'>Submit</button>
+      </form>
+    </div>
+  );
+}
