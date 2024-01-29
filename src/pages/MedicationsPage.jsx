f61d3498152679e9c3ce836aee4fee9b479185dc
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-01-29 09:23:11 +0200

    added router

--- src/pages/MedicationsPage.jsx
+++ src/pages/MedicationsPage.jsx
@@ -1,3 +1,10 @@
+import { Link } from 'react-router-dom';
+
 export default function MedicationsPage() {
-  return <div>MedicationsPage</div>;
+  return (
+    <div className='container'>
+      <h1>MedicationsPage</h1>
+      <Link to='/add-pet'>Add pet</Link>
+    </div>
+  );
 }
