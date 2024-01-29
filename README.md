be7779f62ccb0450171d85773fe8253535633233
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-01-29 09:13:48 +0200

    router + pages + header

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Steps to tailwind
 
-1. install router
+1. install router `npm i react-router-dom`
 2. apjuosti su BrowserRouter App
 3. sukurit Routes su Route elementais
 4. sukurti Headeryje nuorodas i
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,11 +1,15 @@
+import { Link, NavLink } from 'react-router-dom';
+
 export default function Header() {
   return (
     <header>
       <div className='container flex items-center justify-between'>
-        <h2 className='text-2xl font-bold'>Logo</h2>
+        <Link to='/' className='text-2xl font-bold'>
+          Logo
+        </Link>
         <nav className='flex gap-2'>
-          <span>Pets</span>
-          <span>Medications</span>
+          <NavLink to='/'>Pets</NavLink>
+          <NavLink to='/medications'>Medications</NavLink>
         </nav>
       </div>
       <hr />
