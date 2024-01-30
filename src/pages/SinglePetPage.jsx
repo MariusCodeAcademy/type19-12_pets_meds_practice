e2974a81ca93d2d87967eaede3f3cf5996c49ae2
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-01-30 08:56:20 +0200

    single pet with logs

--- src/pages/SinglePetPage.jsx
+++ src/pages/SinglePetPage.jsx
@@ -1,1 +1,32 @@
-// /pets/1
+import { useParams } from 'react-router-dom';
+import PageHeader from '../components/UI/PageHeader';
+import useApiData from '../hooks/useApiData';
+import Card from '../components/UI/Card';
+
+const url = 'https://glittery-dull-snickerdoodle.glitch.me/v1/logs';
+
+export default function SinglePetPage() {
+  // gauti petId
+  const { petId } = useParams();
+  console.log('petId ===', petId);
+
+  const [currentPetArr, setCurrentPetArr, isLoading] = useApiData(`${url}/${petId}`);
+  console.log('currentPetArr ===', currentPetArr);
+  const currentPetObj = currentPetArr[0];
+
+  return (
+    <div className='container'>
+      <PageHeader title={`Pet name: ${currentPetObj?.name}`} to='/' linkText='Go back' />
+
+      <ul className='grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+        {currentPetArr.map((logObj) => (
+          <Card key={logObj.status} li>
+            <p className='border-b mb-3'>Log</p>
+            <h3 className='text-2xl font-semibold mb-3'>{logObj.status}</h3>
+            <p>{logObj.description}</p>
+          </Card>
+        ))}
+      </ul>
+    </div>
+  );
+}
